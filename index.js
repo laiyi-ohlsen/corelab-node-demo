65c1e3eb146ca365d0b02e8f97677d490fe0cc50
laiyi-ohlsen <laiyi.ohlsen@gmail.com>
2024-10-13 19:20:04 -0400

    example-node-app

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express(); 
+app.use(express.json())
+
+const port = process.env.PORT || 3001
+
+app.get('/about', (req, res) => {
+    res.json({ data: "response goes here" })
+})
+
+app.listen(port, () => {
+    console.log(`Example listening on port ${port}`)
+})
